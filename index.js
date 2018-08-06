b906d95f1d322697ada85197e9a4fa744c368360
anatoliianatolich <anatoliianatolich@gmail.com>
2018-08-06 15:39:53 +0300

    06_08_2018

--- index.js
+++ index.js
@@ -1,36 +1,76 @@
 //const http = require('http');
 const app = require('express')();//  const app = require('express')() == {const app = require('express'); const app = app();}
 const conf = require('./config/development');
+const bodyParser = require('body-parser');
 
-// http.createServer((req, res)=>{
-// 	res.writeHead(200, { 'Content-Type': 'text/plain' } );
-// 	debugger;
-// 	res.end('working server')
-// }).listen(5050);
-const users = [
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+
+app.use((req, res, next) => {
+	console.log(`${req.url} --> ${req.method} --> ${Date.now()}`);
+	next();
+})
+
+const USERS = [
 	{
-	user: "vlad",
-	password: 123
+		user: "vlad",
+		password: 123
 	},
 	{
-	user: "dima",
-	password: 123
+		user: "dima",
+		password: 123,
+		books: {
+			title: 'js is suck!',
+			author: "php developer"
+		}
 	},
 	{
-	user: "oleg",
-	password: 123
+		user: "oleg",
+		password: 123
 	},
 	{
-	user: "anatolii",
-	password: 123
+		user: "anatolii",
+		password: 123
 	}
 ];
 
-app.get("/users/",(req ,res, next)=> {
-	res.status((200))
-	res.json(users);
-})
+const getUsers = (req, res, next) => {
+	req.users = USERS;
+	next()
+}
 
+const sendUsers = (req, res, next) => {
+	res.status((200))
+	res.json(req.users);
+	req.users = USERS;
+	next();
+}
+
+const addUser = (req, res, next) => {
+	const user = req.body;
+	USERS.push(user);
+	next();
+}
+// помилка для 404 перевизначена
+app.use((req, res, next) => {
+	let error = new Error('Not found page');
+	next(error);
+});
+
+
+app.get("/users/", getUsers, sendUsers);
+
+app.post("/users/", addUser, sendUsers);
+
+//const users = await getUsers(); варіант доступу до масива
+
+app.use((err, req, res, next) => {
+	res.status(500);
+	res.json({
+		error: err.message,
+		stack: err.stack
+	})
+})
 app.listen(conf.port);
 console.log('server listen port 5050');
 
