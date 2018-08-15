42f95ffbbab909d819e1bbb99f5ab98be47c639c
Anatolii Liutenko <anatoliianatolich@gmail.com>
2018-08-15 23:22:43 +0300

    15_08_18

---
+++ mock-data/users.js
@@ -0,0 +1,31 @@
+const slug = require('slug');
+
+[{
+    "user": "vlad",
+    "password": 123,
+    "books": {
+        "title": "java is suck!",
+        "author": "js developer"
+    }
+}, {
+    "user": "dima",
+    "password": 123,
+    "books": {
+        "title": "js is suck!",
+        "author": "php developer"
+    }
+}, {
+    "user": "oleg",
+    "password": 123,
+    "books": {
+        "title": "php is suck!",
+        "author": "js developer"
+    }
+}, {
+    "user": "anatolii",
+    "password": 123,
+    "books": {
+        "title": "python is nice!",
+        "author": "js developer"
+    }
+}]
