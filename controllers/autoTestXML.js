e9b1f574f609a89bb97bf7f731c1b740749bb8e8
anatoliianatolich <anatoliianatolich@gmail.com>
2018-11-30 17:18:48 +0200

    1

--- controllers/autoTestXML.js
+++ controllers/autoTestXML.js
@@ -5,34 +5,57 @@ const request = require("request");
 const fs = require("fs");
 
 
-module.exports.postXML = (req,res,next) => {
-        // correct query option => https://api.intertop.ua/api/productimages
-request("https://api.intertop.ua/api/productimages/", (err, response, body)=> {
-    res.status(200);
-    res.end(body);
-})
+module.exports.postXML = (req, res, next) => {
+    let id = fs.readFileSync('./page/articles.txt', "utf-8", (err, data) => {
+        if (err) return res.status(400).send(err);
+        return data.split("\b");
+    });
+    let articleProd = id.split("\r");
+    articleProd.forEach((el, i) => {
+        articleProd[i] = el.trim();
+    })
+
+    // console.log(arr[2]);
+    articleProd.forEach((el) => {
+        let XML = `<?xml version="1.0" encoding="utf-8"?><request><products><salecode>` + `${el}` + `</salecode><active>1</active></products></request>`;
+        var options = {
+            url: 'https://api.intertop.ua/api/productimages/',
+            headers: {
+                'content-type': "application/xml"
+            },
+            method: "POST",
+            body: XML
+        }
+
+        request(options, (err, response, body) => {
+            if(err) return res.status(400).send("")
+            console.log(el + "\n"+ body);
+        })
+        res.status(200).end("good work");
+    })
+
 }
 
-module.exports.getReq = (req,res,next) => {
-   
-    let id = fs.readFileSync('./page/cvet.txt', "utf-8",(err,data)=> {
+module.exports.getReq = (req, res, next) => {
+
+    let id = fs.readFileSync('./page/cvet.txt', "utf-8", (err, data) => {
         console.log(1);
-        if(err) return res.status(400).send(err);
+        if (err) return res.status(400).send(err);
         return data.split("\b");
     });
     let arr = id.split("\r");
-    arr.forEach((ei,i)=> {
-        arr[i] = arr[i].trim(); 
+    arr.forEach((ei, i) => {
+        arr[i] = arr[i].trim();
     })
     console.log(arr[2]);
-    arr.forEach((el, i)=> {
-        request(`http://10.18.0.181:7080/kvinto_good/in?GOOD?${el[i]}`, (err, response, body)=> {
-        // if(!body) return res.status(400).end("no connect is KVINTO");
-       let a = 0;
-       a++; 
-        if(a == 10) return res.status(400).end("no connect is KVINTO")
-        console.log(a + "count",i, body, arr[i]);
-    });
+    arr.forEach((el, i) => {
+        request(`http://10.18.0.181:7080/kvinto_good/in?GOOD?${el[i]}`, (err, response, body) => {
+            // if(!body) return res.status(400).end("no connect is KVINTO");
+            let a = 0;
+            a++;
+            if (a == 10) return res.status(400).end("no connect is KVINTO")
+            console.log(a + "count", i, body, arr[i]);
+        });
     })
     res.status(200).end("GOOD");
 }
